1938d9b24a2fdf968857e574e2380003b4b6a180
TranSang231 <dinhsang2313@gmail.com>
2023-08-09 14:34:59 +0700

    create string.js file

---
+++ string.js
@@ -0,0 +1,7 @@
+// string
+let str = `Tran Van Dinh Sang`
+console.log("My name is " + str)  // My name is Tran Van Dinh Sang
+console.log(`My name is ${str}`)  // My name is Tran Van Dinh Sang
+
+console.log(`My name is ${str}
+I am a student`) // My name is Tran Van Dinh Sang, I am a student
