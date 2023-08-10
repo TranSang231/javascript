fb6d593990dbc97a7e604ab840798376c3a6c309
TranSang231 <dinhsang2313@gmail.com>
2023-08-10 15:37:21 +0700

    update object.js file

--- object.js
+++ object.js
@@ -1,4 +1,4 @@
-// create user object
+// create user object by using literal syntax   
 const user = {
     firstName: 'Sang',
     lastName: 'Tran',
@@ -8,6 +8,17 @@ const user = {
         return `My name is ${this.firstName} ${this.lastName}, I'm ${this.age} years old and I am a ${this.job}  `;
     }
 }
+
+// initialize an object by new Object() syntax  
+const fruits = new Object();
+fruits.fruitName = 'watermelon'
+console.log(fruits)
+
+// initialize an object by Object.create() syntax base on user object
+const friend = Object.create(user)
+friend.name = 'Vinh'
+console.log(friend)
+
 
 console.log(user.intro())
 console.log(user.firstName)
@@ -19,3 +30,16 @@ console.log(user)
 // delete job property
 delete user.job
 console.log(user)
+
+
+function Phone(name, version, newFeature) {
+    this.name = name
+    this.version = version
+    this.newFeature = newFeature
+}
+
+const iPhone = new Phone('Iphone', 14, 'Super Retina XDR ')
+const samSung = new Phone('SamSung', 's23 ultra', 'Super HDR selfie camera')
+
+console.log(iPhone)
+console.log(samSung)
